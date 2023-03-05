f0a59b403e7156db7d966536706a90c0ec32a3db
Iurii Bardych <iurii_bardych@hotmail.com>
2023-03-05 16:35:31 +0100

    updates firebase and modal ingredient

--- src/scripts/modal-authentication.js
+++ src/scripts/modal-authentication.js
@@ -2,6 +2,10 @@ const refs = {
   openModalBtn: document.querySelector('[data-modal-authentication-open]'),
   closeModalBtn: document.querySelector('[data-modal-authentication-close]'),
   modal: document.querySelector('[data-modal-authentication]'),
+  joinModalBtn: document.querySelector('[data-modal-authentication-join]'),
+  loginModalBtn: document.querySelector('[data-modal-authentication-login]'),
+  loginForm: document.querySelector('#login-form'),
+  joinForm: document.querySelector('#join-form'),
 };
 
 refs.openModalBtn.addEventListener('click', () => {
@@ -10,4 +14,14 @@ refs.openModalBtn.addEventListener('click', () => {
 
 refs.closeModalBtn.addEventListener('click', () => {
   refs.modal.classList.toggle('is-hidden');
+});
+
+refs.joinModalBtn.addEventListener('click', () => {
+  refs.loginForm.classList.toggle('visually-hidden');
+  refs.joinForm.classList.toggle('visually-hidden');
+});
+
+refs.loginModalBtn.addEventListener('click', () => {
+  refs.loginForm.classList.toggle('visually-hidden');
+  refs.joinForm.classList.toggle('visually-hidden');
 });
