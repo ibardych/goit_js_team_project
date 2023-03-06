049c69b1d0c1b5bdf92fca0e9ac3e39024efbcf8
Anton Tsyrkunov <antontsyrkunov@gmail.com>
2023-03-06 19:59:22 +0200

    propper functions

--- src/hero_js/hero_index.js
+++ src/hero_js/hero_index.js
@@ -1,19 +1,33 @@
-// import { createRequstByLetter } from "./hero_api";
+import { createRequstByLetter } from "./hero_api";
 
-// const alphabetButton = document.querySelector('.selector');
-// const alphabetContainer = document.querySelector('.container-alphabet');
+const alphabetButton = document.querySelector('.selector');
+const alphabetContainer = document.querySelector('.container-alphabet');
+const selector = document.querySelector('.custom-select');
 
-// alphabetContainer.addEventListener('click', onLetterClick);
+alphabetContainer.addEventListener('click', onLetterClick);
+selector.addEventListener('change', onLetterChoose);
 
-//  export function onLetterClick (evt) {
+ export function onLetterClick(evt) {
 
-//     if (evt.target.nodeName !== 'BUTTON') {
-//         return
-//     }
+    if (evt.target.tagName !== 'BUTTON') {
+        return
+    }
 
-//     const currentLetter  = alphabetButton.textContent;     
+
+    const currentLetter  = evt.target.textContent;    
     
-//     createRequstByLetter(currentLetter);
-// }
+    console.log(currentLetter);
+    
+    createRequstByLetter(currentLetter).then(data => console.log(data));
+}
 
+function onLetterChoose(evt) {
+
+    const selectedOption = evt.target.options[evt.target.selectedIndex];
+
+    const selectedOptionText = selectedOption.textContent;
+
+    createRequstByLetter(selectedOptionText).then(data => console.log(data));
+
+  }
 
