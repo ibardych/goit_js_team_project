8041e0976471f03c7aad583ed6475d4a097896f1
Vadym Zimich <tunego97@gmail.com>
2023-03-07 03:13:48 +0200

    add mobile menu + fix some bugs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './scripts/custom-select';
 import './scripts/firebase';
+import './scripts/modal-mob-menu';
 import './scripts/modal-ingredient';
 import './scripts/modal-cocktail';
 import './scripts/modal-authentication';
